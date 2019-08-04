0a9ad8847e60f58db700cd00cd9a2d35cde2124a
Denker <duzhenallan@foxmail.com>
2019-08-04 17:54:17 +0800

    add getCategory(),analyzeImg

--- miniprogram/pages/storageConsole/storageConsole.js
+++ miniprogram/pages/storageConsole/storageConsole.js
@@ -9,6 +9,7 @@ Page({
     cloudPath: '',
     imagePath: '',
     tempFileURL: '',
+    itemCategory: 'AI拼命查询中...'
   },
 
   onLoad: function (options) {
@@ -26,10 +27,63 @@ Page({
       imagePath,
       tempFileURL,
     })
-
     console.group('文件存储文档')
     console.log('https://developers.weixin.qq.com/miniprogram/dev/wxcloud/guide/storage.html')
     console.groupEnd()
+    this.analyzeImg(tempFileURL)
   },
 
+  getCategory: function () {
+    var tagsTobeCate = getApp().globalData.tagsTobeCate
+    var rubbish_table = getApp().globalData.rubbish_table
+    var found = 0
+    var msg = ""
+    for (var i = 0; i < tagsTobeCate.length; i++) {
+      for (var j = 0; j < rubbish_table.length; j++) {
+        if (rubbish_table[j].item == tagsTobeCate[i]) {
+          msg = msg + ( tagsTobeCate[i] + "是" + rubbish_table[j].tag + '\n' )
+          found = 1
+          break
+        }
+      }
+    }
+    if (0 == found)
+      msg = "AI未识别哦，换个角度呗：）"
+    console.log(msg)
+    this.setData ({
+      itemCategory : msg
+    })
+  },
+
+  analyzeImg: function (imgUrl) {
+    // 根据区域和Url分析图片
+    let that = this
+    wx.cloud.callFunction({
+      name: 'getImageLabels',
+      data: {
+        Region: 'ap-beijing',
+        ImageUrl: imgUrl
+      },
+      success: function (res) {
+        console.log(res.result.Labels)
+        var labels = res.result.Labels
+        var tags = []
+        // 设置置信度过滤阈值
+        var confidence = 20
+        for (var idx in labels) {
+          var label = labels[idx]
+          console.log()
+          if (label["Confidence"] >= confidence) {
+            tags.push(label["Name"])
+          }
+        }
+        app.globalData.tagsTobeCate = tags
+        console.log(app.globalData.tagsTobeCate)
+        that.getCategory()
+      },
+      fail: function (err) {
+        console.log(err)
+      }
+    })
+  }
 })
