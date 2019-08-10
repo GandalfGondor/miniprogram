2b53257ef787ba8d46e48ddf18666523555594c7
Denker <duzhenallan@foxmail.com>
2019-08-10 21:23:08 +0800

    maybe done

--- miniprogram/pages/storageConsole/storageConsole.js
+++ miniprogram/pages/storageConsole/storageConsole.js
@@ -10,8 +10,10 @@ Page({
     imagePath: '',
     tempFileURL: '',
     //itemCategory: 'AI拼命查询中...',
-    categories: {},
+    categories :{},
     msgs : [],
+    nr : 0,
+    iconMsg : ''
   },
 
   onLoad: function (options) {
@@ -41,8 +43,9 @@ Page({
     var found = 0
     var msg = ""
     var ms = []
-    var cates = {"有害垃圾":0, "厨余垃圾": 0 ,"其他垃圾": 0 ,"可回收垃圾": 0}
-                
+    var isg = ''
+    var cates = {"有害垃圾":0, "厨余垃圾":0,"其他垃圾": 0,"可回收垃圾":0}
+
     for (var i = 0; i < tagsTobeCate.length; i++) {
       for (var j = 0; j < rubbish_table.length; j++) {
         if (rubbish_table[j].item == tagsTobeCate[i]) {
@@ -56,13 +59,24 @@ Page({
     }
     if (0 == found)
       msg = "AI未识别哦，换个角度呗：）"
-    console.log(msg)
+    if (ms.length == 1) {
+      if (cates["有害垃圾"] == 1) 
+        isg = '有害垃圾指对人体健康或者自然环境造成直接或者潜在危害的生活废弃物，含有有害重金属或有毒物质，常见包括废电池、废荧光灯管、废灯泡、废水银温度计等等。'
+      if (cates["厨余垃圾"] == 1)
+        isg = '厨余垃圾即易腐垃圾，又称湿垃圾，有机垃圾，指易腐烂的生活废弃物。'
+      if (cates["其他垃圾"] == 1)
+        isg = '其他垃圾指除可回收物、有害垃圾、湿垃圾以外的其它生活废弃物。投入干垃圾收集容器，并保持周边环境整洁。'
+      if (cates["可回收垃圾"] == 1)
+        isg = '可回收物就是可以再生循环利用的垃圾，主要包括废纸、塑料、玻璃、金属和布料五大类。'
+    }
+ 
     this.setData ({
       msgs : ms,
-      categories: cates
+      categories: cates,
+      nr : ms.length,
+      iconMsg : isg
     })
-    console.log(cates)
-    console.log(ms)
+
   },
 
   analyzeImg: function (imgUrl) {
@@ -95,5 +109,26 @@ Page({
         console.log(err)
       }
     })
+  },
+  gotoDry: function() {
+    wx.navigateTo({
+      url: '../baike/dry/dry',
+    })
+  },
+  gotoWet: function () {
+    wx.navigateTo({
+      url: '../baike/wet/wet',
+    })
+  },
+  gotoRec: function () {
+    wx.navigateTo({
+      url: '../baike/recyclable/recyclable',
+    })
+  },
+  gotoHam: function () {
+    wx.navigateTo({
+      url: '../baike/harmful/harmful',
+    })
   }
-})
+}) 
+
