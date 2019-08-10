f93d933852ce2a7ed3022666410f7189c02a362d
jojoliang <jojoliang@tencent.com>
2019-08-11 00:36:48 +0800

    update personal

--- miniprogram/pages/personal/personal.js
+++ miniprogram/pages/personal/personal.js
@@ -1,5 +1,22 @@
 const app = getApp()
-
+  /* in app.json
+  "tabBar": {
+    "list": [
+      {
+        "pagePath": "pages/index/index",
+        "iconPath": "images/icon_home.png",
+        "selectedIconPath": "images/icon_home.png",
+        "text": "Home"
+      },
+      {
+        "pagePath": "pages/personal/personal",
+        "iconPath": "images/icon_personal.png",
+        "selectedIconPath": "images/icon_personal.png",
+        "text": "Mine"
+      }
+    ]
+  },
+  */
 Page({
   /**
    * 页面的初始数据
@@ -22,16 +39,20 @@ Page({
   },
 
   onDetail: function(param) {
+    var status = "未达成"
+    var img = param.currentTarget.dataset.img_todo;
+    if (app.globalData.gTotalScan >= param.currentTarget.dataset.scanneed && app.globalData.gTotalScore >= param.currentTarget.dataset.scoreneed) {
+      status = "已达成"
+      img = param.currentTarget.dataset.img_done
+    }
     this.setData({
-      modalHidden: false
+      modalHidden: false,
+      window_title: param.currentTarget.dataset.name,
+      achieve_status : status,
+      window_text1: '扫垃圾数: ' + app.globalData.gTotalScan + '/' + param.currentTarget.dataset.scanneed,
+      window_text2: '游戏得分: ' + app.globalData.gTotalScore + '/' + param.currentTarget.dataset.scoreneed,
+      window_img: img
     })
-    /*
-    console.log(param)
-    wx.showModal({
-      title: param.currentTarget.dataset.name,
-      content: '扫垃圾数: ' + param.currentTarget.dataset.scanneed + '\r\n游戏得分: ' + param.currentTarget.dataset.scoreneed,
-    })
-    console.log(param.currentTarget.dataset) */
   },
 
   modalCandel: function () {
